2592bda8dd327f86a7d9c45d024a26bc98f8ee1a
Quinn <quinn@berylinabox.(none)>
2007-04-05 12:49:59 -0400

    finish (afaik) schema generator, should work properly now, needs testing of course

--- src/schema_generator.c
+++ src/schema_generator.c
@@ -18,6 +18,34 @@
 
 #include "bcop.h"
 
+static char *edgeName[] = {
+	"Left",
+	"Right",
+	"Top",
+	"Bottom",
+	"TopLeft",
+	"TopRight",
+	"BottomLeft",
+	"BottomRight"
+};
+
+int nEdges=sizeof(edgeName)/sizeof(edgeName[0]);
+
+enum {
+	actionPartKey,
+	actionPartButton,
+	actionPartBell,
+	actionPartEdge,
+};
+
+char * actPartInfo[][3]={
+	{"key","Key","Key Binding"},
+	{"button","Button","Mouse Button Binding"},
+	{"bell","Bell","System Bell Binding"},
+	{"edge","Edge","Screen Edge Binding"},
+};
+int nActParts=sizeof(actPartInfo)/sizeof(actPartInfo[0]);
+
 static FILE * schemaFile;
 
 //Nice little function from gconf-dump.c to pretty-print output
@@ -56,9 +84,23 @@ void gconfWriteFooter()
 
 //swiped from gconf-dump.c
 static char *
-gconfTypeToString (OptionType type)
+gconfTypeToString (OptionType type, int actionPart)
 {
     switch (type) {
+		case OptionTypeAction:
+			switch(actionPart)
+			{
+				case actionPartBell:
+					return "bool";
+				case actionPartKey:
+				case actionPartButton:
+					return "string";
+				case actionPartEdge:
+					return "list";
+				default:
+					break;
+			}
+			break;
     case OptionTypeBool:
 	return "bool";
     case OptionTypeString:
@@ -80,11 +122,109 @@ gconfTypeToString (OptionType type)
     return "unknown";
 }
 
-int gconfGetDefaultValue (Option * o, char * value)
+static char * modifiers[] = {
+	"Shift",
+	"Control",
+	"Mod1",
+	"Mod2",
+	"Mod3",
+	"Mod4",
+	"Mod5",
+	"Alt",
+	"Meta",
+	"Super",
+	"Hyper",
+	"ModeSwitch",
+};
+
+int nModifiers=sizeof(modifiers)/sizeof(modifiers[0]);
+
+char * gconfConvertModifiers(char * value, char * ret)
+{
+	char tempBuffer[8192];
+	char tokBuffer[8192];
+	char valBuffer[8192];
+	strcpy(tokBuffer,value);
+	char * tok;
+	ret[0]='\0';
+	for (tok=strtok(tokBuffer,"+");tok;tok=strtok(NULL,"+"))
+	{
+		strcpy(tempBuffer,ret);
+		int i;
+		for (i=0;i<nModifiers;i++)
+		{
+			if (!strcmp(tok,modifiers[i]))
+			{
+				sprintf(ret,"%s<%s>",tempBuffer,tok);
+				break;
+			}
+		}
+		if (i==nModifiers)
+			strcpy(valBuffer,tok);
+	}
+	strcpy(tempBuffer,ret);
+	sprintf(ret,"%s%s",tempBuffer,valBuffer);
+	return ret;	
+}
+
+int gconfGetDefaultValue (Option * o, char * value, int actionPart)
 {
 	OptionValuesList * l;
+	int i;
+	char tempBuffer[8192];
 	switch (o->type)
 	{
+		case OptionTypeAction:
+			switch(actionPart)
+			{
+				case actionPartKey:
+					if (o->data.asBind.skey)
+						sprintf(value,"%s",
+								gconfConvertModifiers(
+									o->data.asBind.key,
+									tempBuffer));
+					else
+						sprintf(value,"None");
+					return 1;
+				case actionPartButton:
+					if (o->data.asBind.smouse)
+						sprintf(value,"%s",
+								gconfConvertModifiers(
+									o->data.asBind.mouse,
+									tempBuffer));
+					else
+						sprintf(value,"None");
+					return 1;
+				case actionPartBell:
+					sprintf(value,
+							(o->data.asBind.sbell?
+							 (o->data.asBind.bell?"true":"false"):"false"));
+					return 1;
+				case actionPartEdge:
+					if (o->data.asBind.sedge)
+					{
+						tempBuffer[0]='\0';
+						for (i=0;i<nEdges;i++)
+						{
+							strcpy(value,tempBuffer);
+							if (o->data.asBind.edge & 1<<i)
+							{
+								if (tempBuffer[0])
+									sprintf(tempBuffer,"%s,%s",value,
+											edgeName[i]);
+								else
+									sprintf(tempBuffer,"%s",edgeName[i]);
+							}
+						}
+						sprintf(value,"[%s]",tempBuffer);
+					}
+					else
+						sprintf(value,"[]");
+					return 1;
+				default:
+					break;
+			}
+			break;
 		case OptionTypeInt:
 			sprintf(value,"%d",o->data.asInt.def);
 			return 1;
@@ -108,7 +248,6 @@ int gconfGetDefaultValue (Option * o, char * value)
 			sprintf(value,"%s",o->data.asMatch.def);
 			return 1;
 		case OptionTypeEnum:
-			//stored in a list
 			for(l=o->data.asSList.begin;l;l=l->next)
 			{
 				if (l->def)
@@ -116,18 +255,42 @@ int gconfGetDefaultValue (Option * o, char * value)
 					return 1;
 			}
 			break;
+		case OptionTypeStringList:
+		case OptionTypeSelection:
+			if (!o->data.asSList.begin)
+			{
+				sprintf(value,"[]");
+				return 1;
+			}
+			tempBuffer[0]='\0';
+			for (l=o->data.asSList.begin;l;l=l->next)
+			{
+				if (o->type==OptionTypeStringList || l->def)
+				{
+					strcpy(value,tempBuffer);
+					if (value[0])
+						sprintf(tempBuffer,"%s,%s",value,l->value);
+					else
+						sprintf(tempBuffer,"%s",l->value);
+				}
+			}
+			sprintf(value,"[%s]",tempBuffer);
+			return 1;
 		default:
 			break;
 	}
 	return 0;
 }
 
-int gconfGetSubInfo (Option * o, char * subInfo)
+int gconfGetSubInfo (Option * o, char * subInfo, int actionPart)
 {
 	OptionValuesList * l;
 	char tempBuffer[8192];
 	switch (o->type)
 	{
+		case OptionTypeAction:
+			sprintf(subInfo,"%s",actPartInfo[actionPart][2]);
+			return 1;
 		case OptionTypeInt:
 			sprintf(subInfo,"%d-%d",o->data.asInt.min,o->data.asInt.max);
 			return 1;
@@ -135,6 +298,7 @@ int gconfGetSubInfo (Option * o, char * subInfo)
 			sprintf(subInfo,"%f-%f",o->data.asFloat.min,o->data.asFloat.max);
 			return 1;
 		case OptionTypeEnum:
+		case OptionTypeSelection:
 			if (!o->data.asSList.begin)
 				return 0;
 			subInfo[0]='\0';
@@ -153,26 +317,37 @@ int gconfGetSubInfo (Option * o, char * subInfo)
 	return 0;
 }
 
-void gconfDumpToSchema(Option * o, char * pathFmt, char * subkey)
+void gconfDumpToSchema(Option * o, char * pathFmt, char * subkey,
+		int actionPart)
 {
 	char path[1024];
 	char subInfo[8192];
 	char value[8192];
 	sprintf(path,pathFmt,subkey,o->name);
+	if (o->type == OptionTypeAction)
+	{
+		strcpy(subInfo,path);
+		sprintf(path,"%s_%s",subInfo,actPartInfo[actionPart][0]);
+	}
 	gconfPrintf(2,"<schema>");
 	gconfPrintf(3,"<key>/schemas%s</key>",path);
 	gconfPrintf(3,"<applyto>%s</applyto>",path);
 	gconfPrintf(3,"<owner>compiz</owner>");
-	gconfPrintf(3,"<type>%s</type>",gconfTypeToString(o->type));
-	if (o->type == OptionTypeStringList)
+	gconfPrintf(3,"<type>%s</type>",gconfTypeToString(o->type,actionPart));
+	if (o->type == OptionTypeStringList ||
+			(o->type == OptionTypeAction && actionPart==actionPartEdge))
 		gconfPrintf(3,"<list_type>string</list_type>");
-	if (gconfGetDefaultValue(o,value))
+	if (gconfGetDefaultValue(o,value,actionPart))
 		gconfPrintf(3,"<default>%s</default>",value);
 	else
 		gconfPrintf(3,"<!-- type not implemented yet -->");
 	gconfPrintf(3,"<locale name=\"C\">");
-	gconfPrintf(4,"<short>%s</short>",o->shortDesc);
-	if(gconfGetSubInfo(o,subInfo))
+	if (o->type == OptionTypeAction)
+		gconfPrintf(4,"<short>%s %s</short>",o->shortDesc,
+				actPartInfo[actionPart][1]);
+	else
+		gconfPrintf(4,"<short>%s</short>",o->shortDesc);
+	if(gconfGetSubInfo(o,subInfo,actionPart))
 		gconfPrintf(4,"<long>%s (%s)</long>",o->longDesc,subInfo);
 	else
 		gconfPrintf(4,"<long>%s</long>",o->longDesc);
@@ -195,10 +370,18 @@ void gconfWriteSchema(int is_general)
 	Option * o = data.options;
 	while (o)
 	{
-		if (o->screen)
-			gconfDumpToSchema(o, pathFmt, "screen0");
+		if (o->type==OptionTypeAction)
+		{
+			int i;
+			for (i=0;i<nActParts;i++)
+				gconfDumpToSchema(o, pathFmt, 
+						(o->screen?"screen0":"allscreens"),i);
+		}
 		else
-			gconfDumpToSchema(o, pathFmt, "allscreens");
+		{
+			gconfDumpToSchema(o, pathFmt, 
+					(o->screen?"screen0":"allscreens"),0);
+		}
 		o=o->next;
 	}
 	gconfWriteFooter();
